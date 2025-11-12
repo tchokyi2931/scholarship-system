727f4d5142abd9141c2add7dd5e54b99f041720f
Tenzin Chokyi <tchokyi2931@gmail.com>
2025-11-12 15:07:13 +0530

    Initial Laravel project setup with basic folder structure

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'course'];
+
+    public function scholarships()
+    {
+        return $this->belongsToMany(Scholarship::class);
+    }
 }
