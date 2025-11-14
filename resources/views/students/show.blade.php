f7252551a43d70a8686691d6465ea93a86e5cd14
Tenzin Chokyi <tchokyi2931@gmail.com>
2025-11-14 22:56:33 +0530

    Implemented student CRUD views and controller logic

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -7,6 +7,13 @@
     <h1>{{ $student->name }}</h1>
     <p><strong>Email:</strong> {{ $student->email }}</p>
     <p><strong>Course:</strong> {{ $student->course }}</p>
+    <p><strong>Scholarships:</strong></p>
+<ul>
+    @foreach ($student->scholarships as $scholarship)
+        <li>{{ $scholarship->name }} (₹{{ $scholarship->amount }})</li>
+    @endforeach
+</ul>
+
     <a href="{{ route('students.index') }}">Back to List</a>
 </body>
 </html>
