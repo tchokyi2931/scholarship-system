9e1591f44f58dddbe0ed6219d81256b0029945f3
Tenzin Chokyi <tchokyi2931@gmail.com>
2025-11-18 14:32:01 +0530

    dashboard changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ScholarshipController;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view(' ');
 });
 // Route::resource('dashboard', )
 Route::resource('students', StudentController::class);
